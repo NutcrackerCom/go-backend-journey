34e595e658a9afe105847055bbcb4c548d2cf7f9
NutcrackerCom <namber1999@mail.ru>
2026-02-02 12:08:02 +0300

    added doubly linked list mod

--- doubly-linked-list/cmd/demo/main.go
+++ doubly-linked-list/cmd/demo/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/NutcrackerCom/go-backend-journey/doubly-linked-list/dll"
+)
+
+func main() {
+	fmt.Println("demo start")
+	var list dll.List
+	list.push_back("one")
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NutcrackerCom/go-backend-journey
+
+go 1.25.5
