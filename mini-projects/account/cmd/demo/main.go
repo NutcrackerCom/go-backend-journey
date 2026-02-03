ad93c10ccd06599a6a9a39fbdc4c49797bae4ec0
NutcrackerCom <namber1999@mail.ru>
2026-02-03 23:50:07 +0300

    demo main

--- mini-projects/account/cmd/demo/main.go
+++ mini-projects/account/cmd/demo/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/NutcrackerCom/go-backend-journey/mini-projects/account"
+)
+
+func main() {
+	var logger *account.Collector
+	var accUserA account.Account = account.Account{Owner: "A", Balance: 100, log: logger}
+	var accUserB account.Account = account.Account{Owner: "B", Balance: 100}
+	accUserA.Transfer(&accUserB, 35)
+	fmt.Println(accUserA, accUserB)
 }
