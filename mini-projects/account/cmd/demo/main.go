9d0fd1e3410713aecbb7268dc14f7f8befa3fed6
NutcrackerCom <namber1999@mail.ru>
2026-02-10 12:04:51 +0300

    draft

--- mini-projects/account/cmd/demo/main.go
+++ mini-projects/account/cmd/demo/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hi"))
+	read, err := io.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+	fmt.Println(string(read))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9091", nil)
 }
