bbd925bbc6bfc53809a4cb65da73b489ef52505a
NutcrackerCom <namber1999@mail.ru>
2026-07-03 14:39:26 +0300

    Rename informatics task folders

--- informatics_msk/35_search_insert_position/main.go
+++ informatics_msk/35_search_insert_position/main.go
@@ -1,14 +1,27 @@
 func searchInsert(nums []int, target int) int {
+
 	l := -1
+
 	r := len(nums)
+
 	for r-l > 1 {
+
 		m := l + (r-l)/2
+
 		fmt.Println(m)
+
 		if nums[m] < target {
+
 			l = m
+
 		} else {
+
 			r = m
+
 		}
+
 	}
+
    return r
+
 }
